f72e9c2474c288278916edbc4038d92c3123acf9
luckylat <lacklat.key@gmail.com>
2022-07-25 21:37:16 +0900

    chore(template): delete unused defines

--- cpp/template/template.cpp
+++ cpp/template/template.cpp
@@ -34,19 +34,10 @@ const int INF = (1<<30)-1;
 const ll LINF = (1LL<<62LL)-1;
 const double EPS = (1e-10);
 
-#define anyfill(n,s) setw(n) << setfill(s)
-#define loop(s) for(int (i) = 0; (s) > (i); (i)++)
-#define rep(i,q) for(int (i) = 0; (q) > (i); (i)++)
-#define repp(i,n,q) for(int (i) = (n); (q) > (i); (i)++)
-#define dep(i,q) for(int (i) = (q); 0 < (i); (i)--)
+P ar4[4] = {{0,1},{0,-1},{1,0},{-1,0}};
+P ar8[8] = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
 
-#define pb push_back
-#define mkp make_pair
-#define fir first
-#define scn second
 
-P ar4[4] = {mkp(0,1),mkp(0,-1),mkp(1,0),mkp(-1,0)};
-P ar8[8] = {mkp(-1,-1),mkp(-1,0),mkp(-1,1),mkp(0,-1),mkp(0,1),mkp(1,-1),mkp(1,0),mkp(1,1)};
 
 
 template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
