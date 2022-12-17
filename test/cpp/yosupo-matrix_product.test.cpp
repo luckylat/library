473d1c7e1a42a4e2271f49322e108cf103312845
luckylat <lacklat.key@gmail.com>
2022-12-17 16:04:41 +0900

    feat(matrix): wip add matrix

---
+++ test/cpp/yosupo-matrix_product.test.cpp
@@ -0,0 +1,22 @@
+//#define PROBLEM "https://judge.yosupo.jp/problem/matrix_product"
+
+#include "../../cpp/template/template.cpp"
+
+#include "../../cpp/data-structure/mod-int.cpp"
+#include "../../cpp/math/matrix.cpp"
+
+int main(){
+  int n,m,k;cin>>n>>m>>k;
+  mat<long long> A(n,m),B(m,k);
+  for(int i = 0; n > i; i++){
+    for(int j = 0; m > j; j++){
+      cin>>A[i][j];
+    }
+  }
+  for(int i = 0; m > i; i++){
+    for(int j = 0; k > j; j++){
+      cin>>B[i][j];
+    }
+  }
+}
+
